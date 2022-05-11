90a3672d648e7081723dcdcca31508b3c60c28ef
황현지 <hjhwang@payletter.com>
2022-05-11 16:38:17 +0900

    Binary Gap, Distinct 풀이 추가

--- src/BinaryGap/Solution.java
+++ src/BinaryGap/Solution.java
@@ -1,2 +1,45 @@
-package BinaryGap;public class Solution {
+package BinaryGap;
+
+/*
+https://app.codility.com/demo/results/trainingP3VMXU-NEN/
+ */
+public class Solution {
+    public int solution(int N){
+        //1. Integer 클래스 사용
+        //String binaryString = Integer.toBinaryString(N);
+        
+        //2. 함수 직접 구현
+        String binaryString = getBinaryString(N);
+
+        int result = 0, count =0;
+
+        for(char c : binaryString.toCharArray()){
+            if(c == '0')
+                count++;
+            else{
+                result = Math.max(count, result);
+                count = 0;
+            }
+        }
+
+        return result;
+    }
+
+    public static String getBinaryString(int N){
+        String binaryString = "";
+        int n = N;
+
+        while (n > 0 ){
+            binaryString = n % 2 + binaryString;
+            n = n / 2;
+        }
+
+        return binaryString;
+    }
+
+    public static void main(String[] args){
+        Solution s = new Solution();
+        System.out.print(s.solution(9));
+        //System.out.print(getBinaryString(529));
+    }
 }
--- src/Distinct/Solution.java
+++ src/Distinct/Solution.java
@@ -1,2 +1,26 @@
-package Distinct;public class Solution {
+package Distinct;
+
+import java.util.HashSet;
+
+/*
+https://app.codility.com/demo/results/trainingW6Y833-N4Y/
+ */
+public class Solution {
+    public int solution(int[] A){
+        int answer = 0;
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : A ){
+            set.add(num);
+        }
+
+        answer = set.size();
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[]{2,1,1,2,3,1}));
+    }
 }
